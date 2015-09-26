930c06bf0f2675778fed30870252c2fc8cc26bd3
dan brooks <dan@danbrooks.net>
2015-09-26 15:58:08 -0400

    moving files around to prepare for packaging, adds setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open("VERSION", "r") as f:
+    version = f.readline().strip()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='minibus',
+      version=version,
+      description="MiniBus IPC Library",
+      long_description=long_description,
+      url="https://github.com/mrdanbrooks/minibus",
+      author="Dan Brooks",
+      license="Apache v2.0",
+
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords='minibus, publish/subscribe, ipc, development',
+
+      packages=['minibus'],
+
+      install_requires=['jsonschema','netifaces'],
+
+      # These can be installed optionally using
+      # $ pip install -e .[twisted,crypto]
+      extras_require={
+          'twisted': ['Twisted'],
+          'crypto': ['gnupg'],
+      },
+
+      scripts=['mbtt']
+      )
+
+
+
